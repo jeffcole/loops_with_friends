3686de417f43b7bc730276d802c762187065f68b
Jeff Cole <jeffreydonaldcole@gmail.com>
2016-06-15 06:22:14 -0600

    Remove username/password in database config

--- config/dev.exs
+++ config/dev.exs
@@ -35,8 +35,6 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :loops, Loops.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "loops_dev",
   hostname: "localhost",
   pool_size: 10
