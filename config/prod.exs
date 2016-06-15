f6c4dd01263d6a0b8f1efad20a1fb4aa4f4c0ca0
Jeff Cole <jeffreydonaldcole@gmail.com>
2016-06-15 07:02:01 -0600

    Prepare for Heroku deployment
    heroku create
    heroku config:set SECRET_KEY_BASE=$(mix phoenix.gen.secret)
    heroku buildpacks:set https://github.com/gjaldon/phoenix-static-buildpack
    heroku buildpacks:add --index 1 https://github.com/HashNuke/heroku-buildpack-elixir

--- config/prod.exs
+++ config/prod.exs
@@ -13,11 +13,17 @@ use Mix.Config
 # which you typically run after static files are built.
 config :loops, Loops.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [host: System.get_env("HOST"), port: System.get_env("PORT")],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+config :loops, Loops.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 20
 
 # ## SSL Support
 #
@@ -59,7 +65,3 @@ config :logger, level: :info
 # for the new static assets to be served after a hot upgrade:
 #
 #     config :loops, Loops.Endpoint, root: "."
-
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
