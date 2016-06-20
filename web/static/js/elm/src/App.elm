97b941d56bc106a6eaf951bdac8ff4f993808119
Jeff Cole <jeffreydonaldcole@gmail.com>
2016-06-20 11:21:07 -0600

    Graduate to a grown-up Elm `program`

--- web/static/js/elm/src/App.elm
+++ web/static/js/elm/src/App.elm
@@ -7,9 +7,26 @@ import View exposing (..)
 
 main : Program Never
 main =
-  Html.App.beginnerProgram { model = "", view = root, update = update }
+  Html.App.program
+    { init = init
+    , view = root
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+init : (String, Cmd a)
+init =
+  (""
+  , Cmd.none
+  )
 
 
 update : a -> a
 update model =
   model
+
+
+subscriptions : a -> Sub b
+subscriptions model =
+  Sub.none
