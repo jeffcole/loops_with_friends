11d5e092c3912caec141ea9bfa43725db2d5623a
Jeff Cole <jeffreydonaldcole@gmail.com>
2016-06-20 14:12:33 -0600

    Add playing of sound on page load

--- lib/loops/endpoint.ex
+++ lib/loops/endpoint.ex
@@ -9,7 +9,7 @@ defmodule Loops.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :loops, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js sounds favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
--- web/static/js/elm/src/App.elm
+++ web/static/js/elm/src/App.elm
@@ -1,7 +1,9 @@
 module App exposing (main)
 
 
+import Audio exposing (defaultPlaybackOptions)
 import Html.App
+import Task exposing (andThen)
 import View exposing (..)
 
 
@@ -15,16 +17,48 @@ main =
     }
 
 
-init : (String, Cmd a)
+
+-- MODEL
+
+
+init : (String, Cmd Msg)
 init =
   (""
-  , Cmd.none
+  , initSound
   )
 
 
-update : a -> a
-update model =
-  model
+initSound : Cmd Msg
+initSound =
+  let
+    url = "sounds/80s_Back_Beat_01.m4a"
+  in
+    Task.perform PlayFail PlaySucceed (playSound url)
+
+
+playSound : String -> Task.Task String ()
+playSound url =
+  Audio.loadSound url
+  `andThen`
+  (Task.mapError (\_ -> "") << Audio.playSound Audio.defaultPlaybackOptions)
+
+
+
+-- UPDATE
+
+
+type Msg
+  = PlaySucceed ()
+  | PlayFail String
+
+
+update : Msg -> a -> (a, Cmd Msg)
+update msg model =
+  (model, Cmd.none)
+
+
+
+-- SUBSCRIPTIONS
 
 
 subscriptions : a -> Sub b
