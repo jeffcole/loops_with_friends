b82eabc0c05a1e3b353d1a231e418814d9d5a408
Jeff Cole <jeffreydonaldcole@gmail.com>
2016-08-16 14:29:52 -0600

    Update Webpack config
    * Remove unnecessary Elm entry point
    * Resolve Node and local modules

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,7 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
   entry: [
       "./web/static/css/app.scss",
-      "./web/static/js/app.js",
-      "./web/static/js/elm/src/App.elm"
+      "./web/static/js/app.js"
   ],
   output: {
     path: "./priv/static",
@@ -24,7 +23,7 @@ module.exports = {
       },
       {
         test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /node_modules/,
         loader: 'babel',
         query: {
           presets: ['es2015']
@@ -40,6 +39,9 @@ module.exports = {
     new CopyWebpackPlugin([{ from: "./web/static/assets" }]),
     new ExtractTextPlugin('css/app.css', { allChunks: true })
   ],
+  resolve: {
+    modulesDirectories: ["node_modules", __dirname + "/web/static/js"]
+  },
   postcss: function () {
     return [Autoprefixer];
   },
