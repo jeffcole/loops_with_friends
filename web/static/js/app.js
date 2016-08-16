7048db16778eb5ab65d76601b6b276ef0a937c21
Jeff Cole <jeffreydonaldcole@gmail.com>
2016-08-16 12:56:12 -0600

    Remove socket.js

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -3,8 +3,6 @@
 // Local files can be imported directly using relative
 // paths "./socket" or full ones "web/static/js/socket".
 
-// import socket from "./socket"
-
 import elm from "./elm/src/App.elm"
 
 window.Elm = elm
