ddab1bfd6a7fedb0dff8c16fdc44ddad0810e8d9
Jeff Cole <jeffreydonaldcole@gmail.com>
2016-08-18 12:18:35 -0600

    Change icon depending on loop state

--- web/static/elm/src/Loop/View.elm
+++ web/static/elm/src/Loop/View.elm
@@ -11,12 +11,30 @@ import Loop.Types exposing (..)
 root : Model -> Html msg
 root model =
   div [ class "loop" ]
-    [ span [ class "icon" ] []
+    [ span [ class (stateClass model.state) ] []
     , span [] [ text (humanizedName model.name) ]
-    , span [] [ text (toString model.state) ]
+    , span [] [ text (stateString model.state) ]
     ]
 
 
+stateClass : Loop.Types.State -> String
+stateClass state =
+  case state of
+    Loop.Types.NotPlaying -> "not-playing"
+    Loop.Types.Queued -> "queued"
+    Loop.Types.Playing -> "playing"
+
+
+stateString : Loop.Types.State -> String
+stateString state =
+  case state of
+    Loop.Types.NotPlaying -> "Stopped"
+    Loop.Types.Queued -> "Queued"
+    Loop.Types.Playing -> "Playing"
+
+
 humanizedName : String -> String
-humanizedName name =
-  String.split "_" name |> String.join " "
+humanizedName string =
+  string
+  |> String.split "_"
+  |> String.join " "
