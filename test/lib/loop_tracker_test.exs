de1f69a9815ebd87cee7989e533c8d7b5949f9f5
Jeff Cole <jeffreydonaldcole@gmail.com>
2016-08-23 16:41:19 -0600

    Add `LoopTracker` test

---
+++ test/lib/loop_tracker_test.exs
@@ -0,0 +1,18 @@
+defmodule Loops.LoopTrackerTest do
+  use ExUnit.Case
+
+  import Loops.LoopTracker, only: [start_link: 0, next_loop: 0]
+
+  test "cycles the loop list" do
+    start_link
+
+    assert next_loop == "80s_Back_Beat"
+    assert next_loop == "Amsterdam_Layers"
+    assert next_loop == "Synthetic_String_Bass"
+    assert next_loop == "Degenerating_Pitch_Vox"
+    assert next_loop == "Kyoto_Night_Guitar"
+    assert next_loop == "Conga_Groove"
+    assert next_loop == "African_Rain_Caxixi"
+    assert next_loop == "80s_Back_Beat"
+  end
+end
