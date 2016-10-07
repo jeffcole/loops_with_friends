7fdd3c8b09f1a37a5f253d7cc0efa2771ce03063
Jeff Cole <jeffreydonaldcole@gmail.com>
2016-10-07 07:28:38 -0600

    Extract `PageView.current_jam`

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,3 +1,7 @@
 defmodule LoopsWithFriends.PageView do
   use LoopsWithFriends.Web, :view
+
+  def current_jam do
+    Application.get_env(:loops_with_friends, :jam_balancer).current_jam
+  end
 end
