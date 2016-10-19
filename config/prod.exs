7549bef57723844924c82d8014e2639c0083c3c7
Jeff Cole <jeffreydonaldcole@gmail.com>
2016-10-19 12:45:27 -0600

    Update pool size and websocket timeout for Heroku
    heroku config:set POOL_SIZE=10

--- config/prod.exs
+++ config/prod.exs
@@ -23,7 +23,7 @@ config :logger, level: :info
 config :loops_with_friends, LoopsWithFriends.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
-  pool_size: 20
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 # ## SSL Support
 #
--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule LoopsWithFriends.UserSocket do
   channel "jams:*", LoopsWithFriends.JamChannel
 
   ## Transports
-  transport :websocket, Phoenix.Transports.WebSocket
+  transport :websocket, Phoenix.Transports.WebSocket, timeout: 45_000
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
